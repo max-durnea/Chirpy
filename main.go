603d2950d6100ddab186b7a8ad536de235dbcdce
max-durnea <maxim.durnea@mail.ru>
2025-09-11 20:46:56 +0300

    Add metrics handlers

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 import(
 	"net/http"
+	"sync/atomic"
+	"fmt"   
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func handler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -12,13 +18,38 @@ func handler(w http.ResponseWriter, r *http.Request){
 
 
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+    count := cfg.fileserverHits.Load()
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    fmt.Fprintf(w, "Hits: %d", count)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+    cfg.fileserverHits.Store(0)
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.Write([]byte("Counter reset"))
+}
+
 func main(){
 	mux := http.NewServeMux()
 	server := &http.Server{}
 	server.Handler = mux
 	server.Addr = "localhost:8080";
-	mux.Handle("/app/",http.StripPrefix("/app/",http.FileServer(http.Dir("."))))
+	var apiCfg = apiConfig{}
+	fs := http.FileServer(http.Dir("."))
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+
 	mux.HandleFunc("/healthz",handler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+    mux.HandleFunc("/reset", apiCfg.resetHandler)
+
 	server.ListenAndServe()
 	
 }
