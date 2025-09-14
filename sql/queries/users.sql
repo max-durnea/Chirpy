69fde4e9b1b9ac160b327604c770d848c60a02dc
max-durnea <maxim.durnea@mail.ru>
2025-09-14 13:14:31 +0300

    Add hashed_password column to CreateUser function

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,10 +1,11 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
 VALUES(
     $1,
     $2,
     $3,
-    $4
+    $4,
+    $5
 )
 RETURNING *;
 
@@ -15,3 +16,7 @@ DELETE FROM users;
 -- name: GetUserById :one
 
 SELECT * FROM users WHERE id = $1;
+
+-- name: GetUserByEmail :one
+
+SELECT * FROM users WHERE email = $1;
